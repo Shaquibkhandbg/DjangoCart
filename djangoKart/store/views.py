478b893404788c7e998875bebf279400010bf193
Shaquibkhandbg <shaquibkhandbg@gmail.com>
2024-12-07 19:54:27 +0530

    "product_details added"

--- djangoKart/store/views.py
+++ djangoKart/store/views.py
@@ -23,3 +23,22 @@ def store(request,category_slug = None):
     }
     
     return render(request,'store/store.html',context)
+
+
+def product_detail(request,category_slug,product_slug):
+    try:
+        # This ensures that the product belongs to the specified category (using the category's slug).  ||  category__slug=category_slug 
+        
+        #  This ensures that the product has the specific slug (the unique identifier for each product).      ||  slug=product_slug  
+        
+        single_product = Product.objects.get(category__slug=category_slug,slug=product_slug)
+        
+    except Exception as e:
+        raise e   #  404 error if no product is found
+    
+    
+    context = {
+        'single_product':single_product,
+    }
+        
+    return render(request,'store/product_detail.html',context)
